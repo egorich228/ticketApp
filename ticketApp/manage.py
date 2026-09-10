1852746130772749d19fb3191fdd79089a395bea
egorkaaa <egorkhaaa637@gmail.com>
2026-09-10 17:22:59 +0300

    Добавил комментарии

--- ticketApp/manage.py
+++ ticketApp/manage.py
@@ -1,10 +1,10 @@
-#комментарий
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+#импорт библиотек
 import os
 import sys
 
-
+#основная функция - запуск административных команд
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticketSite.settings')
@@ -18,6 +18,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
+#запуск main
 if __name__ == '__main__':
     main()
